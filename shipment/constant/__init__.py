49ec1e43326e038d38f32d1294e4429841e68538
Mayank Gajbhiye <mayank1398@gmail.com>
2022-09-18 12:21:28 +0530

    shipment: added experiment

--- shipment/constant/__init__.py
+++ shipment/constant/__init__.py
@@ -72,3 +72,7 @@ MODEL_PATH_KEY = "model_path"
 # Model Pusher config key
 MODEL_PUSHER_CONFIG_KEY = "model_pusher_config"
 MODEL_PUSHER_MODEL_EXPORT_DIR_KEY = "model_export_dir"
+
+# Experiment keys
+EXPERIMENT_DIR_NAME = "experiment"
+EXPERIMENT_FILE_NAME = "experiment.csv"
